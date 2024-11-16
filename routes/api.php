901d6909b5828a4dd90b00c3780f4133e64b1c50
David Marin <davidmarin2211@gmail.com>
2024-11-16 16:25:15 -0500

    Archivos adicionales

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\reddit\RegisterRedditDataController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('register_reddit_data', [RegisterRedditDataController::class, 'registerRedditData'])->name('register_reddit_data');
